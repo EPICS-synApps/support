49b0930edc8d69aef94ceeb3872f1a76e0525614
timmmooney <mooney@aps.anl.gov>
2007-08-30 18:16:59 +0000

    Added doc strings; support 'ignoreComments'; Note whether a substitutions file is actually used

--- utils/convertIocFiles.py
+++ utils/convertIocFiles.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
-# This software is intended to help convert an ioc directory from one version
-# of synApps to another.  Invoked as a program, it takes two arguments, the
-# old directory and the new directory, both of which are assumed to be ioc
-# directories (i.e., they contain st.cmd, or other .cmd files).  The program
-# parses .cmd files and .substitutions files in the source directory, and
-# accumulates information.  Then it parses .cmd files and .substitutions in
-# the new directory. For each .cmd file in the new directory, the program
-# writes a .cmd.CVT file, which is the .cmd file modified to agree as closely
-# as seems reasonable with information collected from the old directory.
-# Substitutions files are also treated in this way
-#
-# The following legal commands are not recognized, because they don't have
-# parentheses or commas.
-#
-# 'epicsEnvSet EPICS_CA_MAX_ARRAY_BYTES 64008'
-# 'var motorUtil_debug,1'
-# 'iocInit'
+"""
+
+This software is intended to help convert an ioc directory from one version of
+synApps to another.  Invoked as a program, it takes two arguments, the old
+directory and the new directory, both of which are assumed to be ioc directories
+(i.e., they contain st.cmd, or other .cmd files).  The program parses .cmd
+files, .substitutions files, and autosave-request files, in the old directory,
+and accumulates information from them.  Then it parses .cmd files, .substitution
+files, and autosave-request files in the new directory. For each .cmd file in
+the new directory, the program writes a .cmd.CVT file, which is the .cmd file
+modified to agree as closely as seems reasonable with information collected from
+the old directory. Substitutions files are also treated in this way.
+
+The following legal commands are not recognized, because they don't have
+parentheses or commas:
+
+    'epicsEnvSet EPICS_CA_MAX_ARRAY_BYTES 64008'
+    'var motorUtil_debug,1'
+    'iocInit'
+"""
 
 import sys
 import os
@@ -27,19 +30,28 @@ from convertCmdFiles import *
 from convertSubFiles import *
 from convertAutosaveFiles import *
 
-def parseCmdFiles(d, filespec, verbose):
+def parseCmdFiles(d, filespec, verbose, ignoreComments=False):
+	"""Call parseCmdFile() for all files in 'filespec'"""
+
 	cmdFiles = glob.glob(filespec)
 	for fileName in cmdFiles:
-		d = parseCmdFile(d, fileName, max(verbose,0))
+		d = parseCmdFile(d, fileName, max(verbose,0), ignoreComments=ignoreComments)
 	return (d)
 
 def writeNewCmdFiles(d, filespec, verbose):
+	"""Call writeNewCmdFile() for all files in 'filespec'"""
+
 	cmdFiles = glob.glob(filespec)
 	for fileName in cmdFiles:
 		d = writeNewCmdFile(d, fileName, max(verbose,0))
 	return(d)
 
 def collectAllInfo(directory=".", verbose=0):
+	"""Call all functions that collect information from ioc files.
+	Create the data structures, 'cmdFileDicts', 'subFileDict', and 'asDicts',
+	in which to accumulate the information, and return them as a tuple.
+	"""
+
 	cmdFileDicts = cmdFileDictionaries()
 	subFileDict = {}
 	asDicts = {}
@@ -50,6 +62,17 @@ def collectAllInfo(directory=".", verbose=0):
 
 	filespec = os.path.join(directory, "*.substitutions")
 	subFileDict = parseSubFiles(subFileDict, filespec, verbose)
+	# Mark as commented out those substitutions files whose dbLoadTemplate
+	# command is not found, or is commented out, in
+	# cmdFileDicts.dbLoadTemplateDict.  (This is actually a list, for consistency
+	# with other dictionaries.  We'll just look at the first element.)
+	for subFile in subFileDict.keys():
+		if subFile in cmdFileDicts.dbLoadTemplateDict.keys():
+			entry = cmdFileDicts.dbLoadTemplateDict[subFile]
+			subFileDict[subFile].leadingComment = entry[0].leadingComment
+		else:
+			subFileDict[subFile].leadingComment = "#"
+	
 	if (verbose): writeSubFileDictionaries(subFileDict, sys.stdout, 120)
 
 	filespec = os.path.join(directory, "auto*.req")
@@ -69,35 +92,40 @@ def collectAllInfo(directory=".", verbose=0):
 
 usage = """
 Usage:    convertIocFiles.py [options] old_dir [new_dir]
-    options: -v[integer]   (verbose/debug level)
+    option: -v[integer]   (verbose/debug level)
+    option: -c            (ignore comments)
 
 Examples: convertIocFiles.py <old_dir> <new_dir>
     convertIocFiles.py <old_dir>"
     convertIocFiles.py -v1 <old_dir> <new_dir>
 
-Synopsis: convertIocFiles.py examines .cmd files and .substitution
-	files, in old_dir, collecting dbLoadRecords/Template
-	commands, function calls, variable assignments (though not
-	'var' commands), SNL program invocations, script commands,
-	and collections of macro-substitution strings.
+Synopsis: convertIocFiles.py examines .cmd files and .substitution files,
+    in old_dir, collecting dbLoadRecords/Template commands, function
+    calls, variable assignments (though not 'var' commands), SNL program
+    invocations, script commands, and collections of macro-substitution
+    strings.
 
-    If new_dir is specified, new versions of all .cmd files in
-    new_dir (named, e.g., st.cmd.CVT) are created or overwritten.
-    The new files are patched with information extracted from
-    old_dir.  The program notes whether commands in old_dir were
-    commented out, and it uses that information in writing
-    commands for new_dir.
+    If new_dir is specified, new versions of all .cmd files and
+	.substitutions files in new_dir (named, e.g., st.cmd.CVT) are created
+    or overwritten. The new files are patched with information extracted
+    from old_dir.  The program notes whether commands in old_dir were
+    commented out, and it uses that information in writing commands for
+    new_dir.
 
-    The program also creates or overwrites the file
-    'convertIocFiles.out' in the current directory.  This file
-    contains a list of the commands found in old_dir.  If new_dir
-    was specified, only those commands from old_dir that did not
-    find a place in new_dir are listed.
+    The program also creates or overwrites the file 'convertIocFiles.out'
+    in the current directory.  This file contains a list of the commands
+    found in old_dir.  If new_dir was specified, only those commands from
+    old_dir that did not find a place in new_dir are listed.
+	
+Result: Files named <existing-file>.CVT are created or overwritten.  The
+    file 'convertIocFiles.out' is created or overwritten. No other files are
+    modified.
 """
 
 def main():
 	verbose = 0
 	dirArg = 1
+	ignoreComments = 0
 	d = cmdFileDictionaries()
 	subFileDict = {}
 	dd = {}
@@ -105,18 +133,20 @@ def main():
 	if len(sys.argv) < 2:
 		print usage
 	else:
-		if (sys.argv[1][0] == '-'):
-			for i in range(len(sys.argv[1])):
-				print "char %d = '%c'" % (i, sys.argv[1][i])
-				if (sys.argv[1][i] == 'v'):
-					if len(sys.argv[1]) > i+1:
-						i = i + 1
-						verbose = int(sys.argv[1][i])
+		for i in range(1,3):
+			if (sys.argv[i][0] != '-'): break
+			for (j, arg) in enumerate(sys.argv[i]):
+				if (arg == 'v'):
+					if len(arg) > j+1:
+						j += 1
+						verbose = int(arg)
 					else:
 						verbose = 1
-					break
-			dirArg = 2
-
+					break # out of 'for (j, arg)...'
+				elif (arg == 'c'):
+					ignoreComments = 1
+					break # out of 'for (j, arg)...'
+		dirArg = i
 		if (len(sys.argv) > dirArg):
 			oldDir = sys.argv[dirArg]
 			# user specified an old directory
@@ -124,15 +154,25 @@ def main():
 				print "\n'"+oldDir+"' is not a directory"
 				return
 			filespec = os.path.join(oldDir, "*.cmd")
-			d = parseCmdFiles(d, filespec, verbose)
+			d = parseCmdFiles(d, filespec, verbose, ignoreComments=ignoreComments)
 			if (verbose): writeCmdFileDictionaries(d, sys.stdout, False)
 
 			filespec = os.path.join(oldDir, "*.substitutions")
-			subFileDict = parseSubFiles(subFileDict, filespec, verbose)
+			subFileDict = parseSubFiles(subFileDict, filespec, verbose, ignoreComments=ignoreComments)
+			# Mark as commented out those substitutions files whose dbLoadTemplate
+			# command is not found, or is commented out, in
+			# d.dbLoadTemplateDict.  (This is actually a list, for consistency
+			# with other dictionaries.  We'll just look at the first element.)
+			for subFile in subFileDict.keys():
+				if subFile in d.dbLoadTemplateDict.keys():
+					entry = d.dbLoadTemplateDict[subFile]
+					subFileDict[subFile].leadingComment = entry[0].leadingComment
+				else:
+					subFileDict[subFile].leadingComment = "#"
 			if (verbose): writeSubFileDictionaries(subFileDict, sys.stdout, 120)
 
 			filespec = os.path.join(oldDir, "auto*.req")
-			dd = parseAutosaveFiles(dd, filespec, verbose)
+			dd = parseAutosaveFiles(dd, filespec, verbose, ignoreComments=ignoreComments)
 			if (verbose): dd = writeAutosaveDictionaries(dd, sys.stdout, False)
 
 			dirArg = dirArg + 1
@@ -153,17 +193,23 @@ def main():
 			# parse cdCommands or envPaths file
 			file = os.path.join(newDir, "cdCommands")
 			if os.path.isfile(file):
-				d = parse_cdCommands(d, file, verbose)
+				d = parse_cdCommands(d, file, verbose, ignoreComments=ignoreComments)
 			else:
 				file = os.path.join(newDir, "envPaths")
 				if os.path.isfile(file):
-					d = parse_envPaths(d, file, verbose)
+					d = parse_envPaths(d, file, verbose, ignoreComments=ignoreComments)
 
 			filespec = os.path.join(newDir, "auto*.req")
 			dd = writeNewAutosaveFiles(dd, filespec, verbose)
-			writeStandardAutosaveFiles(d, subFileDict, newDir)
+
+			# Write new autosave files from info in new_dir
+			(c, s, a) = collectAllInfo(newDir)
+			writeStandardAutosaveFiles(c, s, newDir)
 
 		reportFile = open("convertIocFiles.out", "w+")
+		reportFile.write("command:\n")
+		for word in sys.argv: reportFile.write("%s " % word)
+		reportFile.write("\n")
 		if (wrote_new_files):
 			writeUnusedCmdFileEntries(d, reportFile)
 			writeUnusedSubFileEntries(subFileDict, reportFile, 128)
