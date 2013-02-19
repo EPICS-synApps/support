a8345baa4e666477b331e8b0d2fb699ef317f0f3
Pete Jemian <prjemian@gmail.com>
2013-02-19 16:43:24 +0000

    MS Windows batch file to checkout the synApps trunk

---
+++ checkout.bat
@@ -0,0 +1,25 @@
+rem checkout.bat
+
+echo off
+
+rem Checkout the synApps trunk from the subversion repository
+
+mkdir synAppsSVN
+cd synAppsSVN
+
+rem For file access
+rem set SVN=file:///home/joule/SVNSYNAP/svn
+
+rem For https access
+set SVN=https://subversion.xray.aps.anl.gov/synApps
+
+svn co %SVN%/support/trunk support
+cd support
+
+set items=areaDetector autosave busy calc camac configure
+set items=%items% dac128V delaygen documentation dxp ebrick
+set items=%items% ip ip330 ipUnidig love mca modbus motor
+set items=%items% optics quadEM sscan softGlue std stream
+set items=%items% utils vac vme vxStats xxx
+
+FOR %%i IN (%items%) DO svn co %SVN%/%%i/trunk %%i
