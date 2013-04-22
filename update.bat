5d7bfa68d8ea96368e3eae2b972213f746700997
timmmooney <mooney@aps.anl.gov>
2013-04-22 17:12:27 +0000

    Added measComp, deleted vxStats

--- update.bat
+++ update.bat
@@ -6,9 +6,9 @@ rem Update from the synApps subversion repository
 
 set items=areaDetector autosave busy calc camac configure
 set items=%items% dac128V delaygen documentation dxp ebrick
-set items=%items% ip ip330 ipUnidig love mca modbus motor
+set items=%items% ip ip330 ipUnidig love mca measComp modbus motor
 set items=%items% optics quadEM sscan softGlue std stream
-set items=%items% utils vac vme vxStats xxx
+set items=%items% utils vac vme xxx
 
 svn up
 FOR %%i IN (%items%) DO svn up %%i
