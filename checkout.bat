e78928bcd0f7d6dde74c3b540a3d608a58d39930
timmmooney <mooney@aps.anl.gov>
2015-01-13 21:02:17 +0000

    caputRecorder

--- checkout.bat
+++ checkout.bat
@@ -16,7 +16,7 @@ set SVN=https://subversion.xray.aps.anl.gov/synApps
 svn co %SVN%/support/trunk support
 cd support
 
-set items= autosave busy calc camac configure
+set items= autosave busy calc camac caputRecorder configure
 set items=%items% dac128V delaygen documentation dxp ebrick
 set items=%items% ip ip330 ipUnidig love mca measComp modbus motor
 set items=%items% optics quadEM sscan softGlue std stream
