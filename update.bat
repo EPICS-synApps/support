8b664514c94eaf425417f93587cfda18d6b5ca1b
timmmooney <mooney@aps.anl.gov>
2015-01-13 21:01:25 +0000

    caputRecorder

--- update.bat
+++ update.bat
@@ -4,7 +4,7 @@ echo off
 
 rem Update from the synApps subversion repository
 
-set items= autosave busy calc camac configure
+set items= autosave busy calc camac caputRecorder configure
 set items=%items% dac128V delaygen documentation dxp ebrick
 set items=%items% ip ip330 ipUnidig love mca measComp modbus motor
 set items=%items% optics quadEM sscan softGlue std stream
