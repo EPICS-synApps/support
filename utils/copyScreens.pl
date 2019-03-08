6c66aa7edcd3bafb97c52fd4f9e7734aba635ad4
Keenan Lang <klang@aps.anl.gov>
2019-03-08 11:59:52 -0600

    Generate all_adl/ui/edl/opi folder

---
+++ utils/copyScreens.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use File::Copy;
+use File::Spec;
+
+sub searchFolder
+{	
+	my $collection = $_[0];
+	my $cur = $_[1];
+	my $ext = $_[2];
+	
+	my @subs = glob(File::Spec->catfile($cur, "*"));
+
+	print"Searching: $cur\n";
+	
+	foreach my $file (@subs)
+	{
+		if (-d $file)
+		{
+			searchFolder($collection, $file, $ext);
+		}
+		elsif (index($file, ".$ext") == (length($file) - length($ext) - 1))
+		{
+			copy $file, $collection or die "File cannot be copied.";
+		}
+	}
+}
+
+
+my $argc = @ARGV;
+
+if ( $argc < 2 || $argc > 2)
+{
+	print "usage: copyScreens top_dir file_ext\n";
+	exit 2;
+}
+
+my $top_dir = $ARGV[0];
+my $file_ext = $ARGV[1];
+my $collection_dir = File::Spec->catdir($top_dir, "all_$file_ext");
+
+mkdir $collection_dir;
+
+searchFolder($collection_dir, $top_dir, $file_ext);
