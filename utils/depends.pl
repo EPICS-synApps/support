b5b91cd83e058f1ed9f00a6232376f7d6909f33d
klang <klang@aps.anl.gov>
2019-04-30 17:18:15 -0500

    add dependency parser

---
+++ utils/depends.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+my $argc = @ARGV;
+
+# Inputs are 1) path to convertRelease.pl, 2) path to module, 3) Macro name of module, 4) list of modules to build against
+
+my $convertrelease = $ARGV[0];
+my $dir = $ARGV[1];
+my $module = $ARGV[2];
+my $module_list = $ARGV[3];
+
+if ( ! -e "${dir}/configure/RELEASE" )
+{
+	print "";
+	exit;
+}
+
+# Parse the configure file
+my @list = split / /, `perl ${convertrelease} -T ${dir} releaseTops`;
+
+my @modules = split / /, $module_list;
+my %modcheck = map { $_ => 1 } @modules;
+
+# Modules shouldn't depend on themselves
+delete($modcheck{$module});
+
+my $output = "";
+
+foreach my $test (@list)
+{	
+	# Only match against the list of modules given
+	if (exists($modcheck{$test}))
+	{
+		# Put $() around the macros
+		$output = $output . "\$\($test\) ";
+	}
+}
+
+print ${output};
