80b228227fd720cc7eeb5ce7c5cc12d172f0c321
klang <klang@aps.anl.gov>
2019-05-29 16:17:09 -0500

    newlines ruined matching

--- utils/depends.pl
+++ utils/depends.pl
@@ -16,7 +16,11 @@ if ( ! -e "${dir}/configure/RELEASE" )
 }
 
 # Parse the configure file
-my @list = split / /, `perl ${convertrelease} -T ${dir} releaseTops`;
+my $data = `perl ${convertrelease} -T ${dir} releaseTops`;
+$data =~ s/\r//g;
+$data =~ s/\n//g;
+
+my @list = split / /, $data;
 
 my @modules = split / /, $module_list;
 my %modcheck = map { $_ => 1 } @modules;
@@ -27,10 +31,14 @@ delete($modcheck{$module});
 my $output = "";
 
 foreach my $test (@list)
-{	
+{
+	
 	# Only match against the list of modules given
 	if (exists($modcheck{$test}))
 	{
+		print $test;
+		print "\n";
+		
 		# Put $() around the macros
 		$output = $output . "\$\($test\) ";
 	}
