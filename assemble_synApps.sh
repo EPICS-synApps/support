a2068f69bffd6df7d8c3f15bf5ecbb257d95c6f0
Keenan Lang <klang@aps.anl.gov>
2021-11-03 15:32:57 -0500

    Remove JSON from all builds until separate DBD

--- assemble_synApps.sh
+++ assemble_synApps.sh
@@ -276,7 +276,8 @@ cp EXAMPLE_RELEASE.local           RELEASE.local
 echo 'WITH_GRAPHICSMAGICK = NO' >> CONFIG_SITE.local.vxWorks
 echo 'WITH_BLOSC = NO' >> CONFIG_SITE.local.vxWorks
 echo 'WITH_BITSHUFFLE = NO' >> CONFIG_SITE.local.vxWorks
-echo 'WITH_JSON = NO' >> CONFIG_SITE.local.vxWorks
+
+sed -i s:'WITH_JSON = YES':'WITH_JSON = NO':g CONFIG_SITE.local
 
 
 # linux-arm has X11 and other issues
